a9a04a6789491f9a2eae991655254235b99b6cc9
AkhyMuss <120144393+gordibus@users.noreply.github.com>
2024-03-19 18:45:56 +0100

    Update unzip.c

--- unzip.c
+++ unzip.c
@@ -5,56 +5,62 @@
 int main() {
     char zip_path[256];
     printf("Veuillez entrer le chemin du fichier ZIP à décompresser : ");
-    scanf("%255s", zip_path); // Limiter la taille de l'entrée à 255 caractères pour éviter un dépassement de tampon
-
-    // Ouvrir le fichier ZIP
+    scanf("%255s", zip_path); 
+    
+    
     int err = 0;
     zip_t *z = zip_open(zip_path, 0, &err);
 
-    if (z == NULL) {
+    if (z == NULL) 
+    {
         fprintf(stderr, "Erreur lors de l'ouverture du fichier ZIP.\n");
         return 1;
     }
 
-    // Obtenir le nombre d'entrées dans le fichier ZIP
+    
     int num_entries = zip_get_num_entries(z, 0);
-    if (num_entries < 0) {
-        fprintf(stderr, "Erreur lors de la récupération du nombre d'entrées dans le fichier ZIP.\n");
+    if (num_entries < 0) 
+    {
+        fprintf(stderr, "Error retrieving number of entries in ZIP file.\n");
         zip_close(z);
         return 1;
     }
 
-    // Parcourir chaque entrée dans le fichier ZIP
-    for (int i = 0; i < num_entries; i++) {
-        // Obtenir les informations sur l'entrée
+    
+    for (int i = 0; i < num_entries; i++) 
+    {
+       
         struct zip_stat st;
         zip_stat_index(z, i, 0, &st);
 
-        // Ouvrir l'entrée
+        
         zip_file_t *f = zip_fopen_index(z, i, 0);
-        if (f == NULL) {
-            fprintf(stderr, "Erreur lors de l'ouverture de l'entrée %d dans le fichier ZIP.\n", i);
+        if (f == NULL) 
+        {
+            fprintf(stderr, "Error opening entry %d in ZIP file.\n", i);
             zip_close(z);
             return 1;
         }
 
-        // Créer un fichier avec le nom de l'entrée
+        
         FILE *output_file = fopen(st.name, "wb");
-        if (output_file == NULL) {
-            fprintf(stderr, "Erreur lors de la création du fichier de sortie pour l'entrée %d.\n", i);
+        if (output_file == NULL) 
+        {
+            fprintf(stderr, "Error creating output file for input %d.\n", i);
             zip_fclose(f);
             zip_close(z);
             return 1;
         }
 
-        // Lire le contenu de l'entrée et écrire dans le fichier de sortie
+       
         char buf[1024];
         zip_int64_t nbytes;
-        while ((nbytes = zip_fread(f, buf, sizeof(buf))) > 0) {
+        while ((nbytes = zip_fread(f, buf, sizeof(buf))) > 0) 
+        {
             fwrite(buf, 1, nbytes, output_file);
         }
 
-        // Fermer les fichiers
+       
         fclose(output_file);
         zip_fclose(f);
     }
@@ -62,7 +68,7 @@ int main() {
     // Fermer le fichier ZIP
     zip_close(z);
 
-    printf("Décompression terminée avec succès.\n");
+    printf("Unzip successful.\n");
 
     return 0;
 }
